4cc6424b6b8851307d88a35d4e2510cc278273e0
mohammed ahmed khan <84978114+mak0551@users.noreply.github.com>
2025-09-23 18:59:27 +0530

    done

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { login, signup } from "../controllers/authController.js";
+import { getbyid, login, signup } from "../controllers/authController.js";
 const router = express.Router();
 
 // Auth routes
 router.post("/signup", signup);
 router.post("/login", login);
+router.post("/getbyid/:id", getbyid);
 
 export default router;
